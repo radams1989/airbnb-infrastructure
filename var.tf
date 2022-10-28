da023302a6d71b9be96cffd5ffaac0a54d8af002
USER_NAME <radams.cj@gmail.com>
2022-10-27 22:38:58 -0300

    adding the infra

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "AMI_ID" {
+    type = string
+    description = "AMI_ID"
+    default = "ami-0149b2da6ceec4bb0"
+  
+}
+variable "INSTANCE_TYPE" {
+    type = string
+    description = "type of ece instance "
+    default = "t2.micro"
+  
+}
