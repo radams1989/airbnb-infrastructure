15bb727e87a55228ec4449dc81f9c2399e3001e8
USER_NAME <radams.cj@gmail.com>
2022-11-06 23:12:16 -0400

    modified s3

--- s3.tf
+++ s3.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "MyBucket" {
     bucket = var.bucket_name 
 }
+
+resource "aws_s3_bucket_public_access_block" "access_MyBucket" {
+  bucket = aws_s3_bucket.MyBucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
